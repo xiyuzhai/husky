1e1f9c668b43053ab7f68434942c49e7cd2e37ed
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-01-17 13:43:27 -0800

    more power signature

--- crates/visored/visored-pipeline/lean4/visored-pipeline-tests/VisoredPipelineTests/batch1/example-8.lean
+++ crates/visored/visored-pipeline/lean4/visored-pipeline-tests/VisoredPipelineTests/batch1/example-8.lean
@@ -162,24 +162,24 @@ def h (a b : ℝ) (h1 : a > (0 : ℝ)) (h2 : b > (0 : ℝ)) : a / b + b / a ≥ 
   have h8 : a / b + b / a ≥ (2 : ℝ) := by
     have d : a = a := by term_derivation_variable
     have d1 : b = b := by term_derivation_variable
-    have d2 : a / b = b * a := by term_derivation_div
+    have d2 : a / b = b ^ (-1) * a := by term_derivation_div
     have d3 : (2 : ℝ) = 2 := by term_derivation_literal
-    have d4 : a / b - (2 : ℝ) = (-2 : ℝ) + b * a := by term_derivation_sub
+    have d4 : a / b - (2 : ℝ) = (-2 : ℝ) + b ^ (-1) * a := by term_derivation_sub
     have d5 : b = b := by term_derivation_variable
     have d6 : a = a := by term_derivation_variable
-    have d7 : b / a = b * a := by term_derivation_div
-    have d8 : a / b - (2 : ℝ) + b / a = (-2 : ℝ) + (2 : ℝ) * b * a := by term_derivation_sum
+    have d7 : b / a = b * (a ^ -1) := by term_derivation_div
+    have d8 : a / b - (2 : ℝ) + b / a = (-2 : ℝ) + b ^ -1 * a + b * (a ^ -1) := by term_derivation_sum
     have d9 : (0 : ℝ) = 0 := by term_derivation_literal
-    have d10 : a / b - (2 : ℝ) + b / a ≥ (0 : ℝ) ↔ (-2 : ℝ) + (2 : ℝ) * b * a ≥ (0 : ℝ) := by term_derivation_chaining_separated_list
+    have d10 : a / b - (2 : ℝ) + b / a ≥ (0 : ℝ) ↔ (-2 : ℝ) + b ^ -1 * a + b * (a ^ -1) ≥ (0 : ℝ) := by term_derivation_chaining_separated_list
     have d11 : a = a := by term_derivation_variable
     have d12 : b = b := by term_derivation_variable
-    have d13 : a / b = b * a := by term_derivation_div
+    have d13 : a / b = b ^ -1 * a := by term_derivation_div
     have d14 : b = b := by term_derivation_variable
     have d15 : a = a := by term_derivation_variable
-    have d16 : b / a = b * a := by term_derivation_div
-    have d17 : a / b + b / a = (2 : ℝ) * b * a := by term_derivation_sum
+    have d16 : b / a = b * (a ^ -1) := by term_derivation_div
+    have d17 : a / b + b / a = b ^ -1 * a + b * (a ^ -1) := by term_derivation_sum
     have d18 : (2 : ℝ) = 2 := by term_derivation_literal
-    have d19 : a / b + b / a ≥ (2 : ℝ) ↔ (-2 : ℝ) + (2 : ℝ) * b * a ≥ (0 : ℝ) := by term_derivation_chaining_separated_list
+    have d19 : a / b + b / a ≥ (2 : ℝ) ↔ (-2 : ℝ) + b ^ -1 * a + b * (a ^ -1) ≥ (0 : ℝ) := by term_derivation_chaining_separated_list
     have d20 : a / b + b / a ≥ (2 : ℝ) := by term_derivation_finalize d10 d19
     assumption
   obvious
