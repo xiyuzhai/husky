0d20d430f5fb0ef9bba86309e0737feeeddc292b
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-01-18 06:54:03 -0800

    save

--- crates/visored/visored-baseq-elaborator/src/derivation/term/chaining_separated_list.rs
+++ crates/visored/visored-baseq-elaborator/src/derivation/term/chaining_separated_list.rs
@@ -13,6 +13,28 @@ impl<'db, 'sess> VdBsqElaboratorInner<'db, 'sess> {
             .iter()
             .map(|&(func, follower)| (func, self.transcribe_expr_term_derivation(follower, hc)))
             .collect::<Vec<_>>();
+        match follower_equivalences.len() {
+            0 => unreachable!(),
+            1 => {
+                let (signature, follower_equivalence) = follower_equivalences[0];
+                self.transcribe_trivial_chaining_separated_list_term_derivation_construction(
+                    leader_equivalence,
+                    signature,
+                    follower_equivalence,
+                    hc,
+                )
+            }
+            _ => todo!(),
+        }
+    }
+
+    fn transcribe_trivial_chaining_separated_list_term_derivation_construction(
+        &mut self,
+        leader_equivalence: VdMirDerivationIdx,
+        signature: VdBaseChainingSeparatorSignature,
+        follower_equivalence: VdMirDerivationIdx,
+        hc: &mut VdMirHypothesisConstructor<'db, VdBsqHypothesisIdx<'sess>>,
+    ) -> VdMirTermDerivationConstruction {
         todo!()
     }
 }
