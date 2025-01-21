e7c408ac9173c3e82eb4b436bdeb170f14a1c82e
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-01-21 08:43:26 -0800

    save

--- crates/visored/visored-mir-expr/src/derivation/construction/term/finish.rs
+++ crates/visored/visored-mir-expr/src/derivation/construction/term/finish.rs
@@ -4,7 +4,10 @@ pub(super) fn check_non_trivial_finish<'db, Src>(
     prop: VdMirExprIdx,
     src_nf: VdMirTermDerivationIdx,
     dst_nf: VdMirTermDerivationIdx,
-    hc: &VdMirHypothesisConstructor<'db, Src>,
+    hc: &mut VdMirHypothesisConstructor<'db, Src>,
 ) {
-    todo!()
+    ds!(let (_src_expr => src_term) = src_nf.prop(hc), hc);
+    ds!(let (dst_expr => dst_term) = dst_nf.prop(hc), hc);
+    assert_deep_eq!(prop, dst_expr, hc);
+    assert_deep_eq!(src_term, dst_term, hc);
 }
