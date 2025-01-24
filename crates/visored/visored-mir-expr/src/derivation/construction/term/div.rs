4e861d8f9bd143c3816ffe763c19cf08272a1dd5
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-01-24 09:31:03 -0800

    save

--- crates/visored/visored-mir-expr/src/derivation/construction/term/div.rs
+++ crates/visored/visored-mir-expr/src/derivation/construction/term/div.rs
@@ -1,6 +1,7 @@
 use super::*;
 use hypothesis::VdMirHypothesisIdx;
 
+/// derive `a / b => term` from `a => a_term`, `b => b_term` and `a_term / b_term => term`
 pub(super) fn check_div_eq<'db, Src>(
     prop: VdMirExprIdx,
     numerator_dn: VdMirTermDerivationIdx,
@@ -8,7 +9,17 @@ pub(super) fn check_div_eq<'db, Src>(
     numerator_dn_div_denominator_dn_dn: VdMirTermDerivationIdx,
     hc: &mut VdMirHypothesisConstructor<'db, Src>,
 ) {
-    todo!()
+    ds!(let (expr => term) = prop, hc);
+    ds!(let (a / b) = expr, hc);
+    ds!(let (a1 => a_term) = numerator_dn.prop(hc), hc);
+    ds!(let (b1 => b_term) = denominator_dn.prop(hc), hc);
+    ds!(let (a_term_div_b_term => term1) = numerator_dn_div_denominator_dn_dn.prop(hc), hc);
+    ds!(let (a_term1 / b_term1) = a_term_div_b_term, hc);
+    assert_deep_eq!(a1, a, hc);
+    assert_deep_eq!(b1, b, hc);
+    assert_deep_eq!(term1, term, hc);
+    assert_deep_eq!(a_term1, a_term, hc);
+    assert_deep_eq!(b_term1, b_term, hc);
 }
 
 /// derive `a / b => term` from `b⁻¹ * a => term` if `b` is a literal
@@ -17,8 +28,8 @@ pub(super) fn check_div_literal<'db, Src>(
     a_mul_b_inv_dn: VdMirTermDerivationIdx,
     hc: &mut VdMirHypothesisConstructor<'db, Src>,
 ) {
-    ds!(let (lhs => term) = prop, hc);
-    ds!(let (a / b) = lhs, hc);
+    ds!(let (expr => term) = prop, hc);
+    ds!(let (a / b) = expr, hc);
     ds!(let (b_inv_mul_a => term1) = a_mul_b_inv_dn.prop(hc), hc);
     ds!(let (b_inv * a1) = b_inv_mul_a, hc);
     assert_deep_eq!(a1, a, hc);
