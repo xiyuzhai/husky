302fce6e18e9947aa09c99555546e6d3d1d3c180
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-01-25 21:39:27 -0800

    save

--- lean4/visored/Visored/Tactics/Derivation/Term.lean
+++ lean4/visored/Visored/Tactics/Derivation/Term.lean
@@ -3,13 +3,12 @@ import Visored.Tactics.Derivation.Term.Sum
 import Visored.Tactics.Derivation.Term.Product
 import Visored.Tactics.Derivation.Term.Coercion
 
-macro "term_derivation_add_eq": tactic => `(tactic| sorry)
-macro "term_derivation_atom_add_non_zero_literal": tactic => `(tactic| sorry)
+macro "term_derivation_add_eq": tactic => `(tactic| fail)
 macro "term_derivation_neg_literal": tactic => `(tactic| norm_num)
-macro "term_derivation_num_comparison": tactic => `(tactic| sorry)
+macro "term_derivation_num_comparison": tactic => `(tactic| fail)
 macro "term_derivation_reflection": tactic => `(tactic| rfl)
-macro "term_derivation_sub_eqs_add_neg": tactic => `(tactic| sorry)
-macro "term_derivation_power_one": tactic => `(tactic| sorry)
-macro "term_derivation_one_mul": tactic => `(tactic| sorry)
-macro "term_derivation_nf_add_zero": tactic => `(tactic| sorry)
-macro "term_derivation_non_trivial_finish": tactic => `(tactic| sorry)
+macro "term_derivation_sub_eqs_add_neg": tactic => `(tactic| fail)
+macro "term_derivation_power_one": tactic => `(tactic| fail)
+macro "term_derivation_one_mul": tactic => `(tactic| fail)
+macro "term_derivation_nf_add_zero": tactic => `(tactic| fail)
+macro "term_derivation_non_trivial_finish": tactic => `(tactic| fail)
--- lean4/visored/Visored/Tactics/Derivation/Term/Sum.lean
+++ lean4/visored/Visored/Tactics/Derivation/Term/Sum.lean
@@ -1,13 +1,22 @@
 import Mathlib
 
-theorem term_derivation_add_atom_thm {α} {a b term: α} [CommRing α] (h: a + 1 * b^1 = term) : a + b = term := by
+theorem term_derivation_add_atom {α} {a b term: α} [CommRing α] (h: a + 1 * b^1 = term) : a + b = term := by
   rw [←h]
   ring
 
 /-- derive `a + b => term` from `a + 1 * b^1 => term` if `b` is an atom
 -/
-macro "term_derivation_add_atom" d:term : tactic => `(tactic| (exact term_derivation_add_atom_thm $d))
+macro "term_derivation_add_atom" d:term : tactic => `(tactic| (exact term_derivation_add_atom $d))
 
 example (x: ℝ) (d18: (-1 : ℝ) + (1 : ℝ) * x ^ 1 = (-1 : ℝ) + (1 : ℝ) * x ^ 1)
   : (-1 : ℝ) + x = (-1 : ℝ) + (1 : ℝ) * x ^ 1 := by
   term_derivation_add_atom d18
+
+theorem term_derivation_atom_add_non_zero_literal {α} {a c: α} [CommRing α] : a + c = c + 1 * a^1 := by
+  ring
+
+/-- derive `a + c => c + 1 * a^1` if `a` is an atom and `c` is a litnum -/
+macro "term_derivation_atom_add_non_zero_literal" : tactic => `(tactic| (exact term_derivation_atom_add_non_zero_literal))
+
+example (x: ℝ) : x + 2 = 2 + 1 * x^1 := by
+  term_derivation_atom_add_non_zero_literal
