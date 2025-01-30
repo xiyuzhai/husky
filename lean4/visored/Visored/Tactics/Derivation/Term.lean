99cae08013616b01b605075fa9f30e91c183f9b1
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-01-30 14:19:20 -0800

    impl lean side term derivation non trivial finish

--- lean4/visored/Visored/Tactics/Derivation/Term.lean
+++ lean4/visored/Visored/Tactics/Derivation/Term.lean
@@ -6,5 +6,11 @@ import Visored.Tactics.Derivation.Term.Coercion
 macro "term_derivation_neg_literal": tactic => `(tactic| norm_num)
 macro "term_derivation_num_comparison": tactic => `(tactic| norm_num)
 macro "term_derivation_reflection": tactic => `(tactic| rfl)
+
+theorem term_derivation_non_trivial_finish {src dst nf : Prop} (hsrc: src) (hsrc_nf: src ↔ nf) (hdst_nf: dst ↔ nf) : dst := by
+  have hnf : nf := hsrc_nf.mp hsrc
+  exact hdst_nf.mpr hnf
+
 /-- derive `dst` from `src`, `src_nf` and `dst_nf` -/
-macro "term_derivation_non_trivial_finish" src:term:1024 src_nf:term:1024 dst_nf:term:1024 : tactic => `(tactic| fail "not implemented")
+macro "term_derivation_non_trivial_finish" src:term:1024 src_nf:term:1024 dst_nf:term:1024 : tactic
+  => `(tactic| exact term_derivation_non_trivial_finish $src $src_nf $dst_nf)
