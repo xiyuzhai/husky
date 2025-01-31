7413169d73898a016255616224233dadb7033c1c
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-01-31 14:58:36 -0800

    save

--- lean4/visored/Visored/Tactics/Derivation.lean
+++ lean4/visored/Visored/Tactics/Derivation.lean
@@ -1,1 +1,2 @@
 import Visored.Tactics.Derivation.Term
+import Visored.Tactics.Derivation.LitnumBound
---
+++ lean4/visored/Visored/Tactics/Derivation/LitnumBound.lean
@@ -0,0 +1,3 @@
+import Mathlib
+
+macro "litnum_bound_derivation_finish" : tactic => `(tactic| fail "not implemented")
